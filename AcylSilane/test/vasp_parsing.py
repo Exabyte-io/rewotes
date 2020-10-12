22e916de6c5438a406339aaf47dc6596c859777e
James <None>
2020-10-12 02:13:26 -0400

    Move Cu VASP inputs into their own folder, to maintain organized directory structure

--- AcylSilane/test/vasp_parsing.py
+++ AcylSilane/test/vasp_parsing.py
@@ -10,13 +10,13 @@ from AcylSilane.convtrack.calculation import Calculation, Convergence
 
 class CalculationTests(unittest.TestCase):
     def setUp(self):
-        self.data_dir = os.path.join(os.path.dirname(os.getcwd()), "data")
+        self.data_dir = os.path.join(os.path.dirname(os.getcwd()), "data/cu_vasp_files")
         self.incar = os.path.join(self.data_dir, "INCAR")
         self.poscar = os.path.join(self.data_dir, "POSCAR")
         self.kpoints = os.path.join(self.data_dir, "KPOINTS")
         self.calc = Calculation(self.incar, self.poscar, dims=(1, 1, 1), kpoints=self.kpoints)
 
-        self.convergence = Convergence(self.incar, self.poscar, max_size=5, kpoints=self.kpoints)
+        self.convergence = Convergence(self.incar, self.poscar, max_size=3, kpoints=self.kpoints)
         self.ase_obj = ase.io.read(self.poscar)
 
     def tearDown(self):
@@ -36,8 +36,7 @@ class CalculationTests(unittest.TestCase):
         (1, 1, 2),
         (1, 2, 1),
         (2, 1, 1),
-        (5, 5, 5),
-        (4, 4, 4)
+        (3, 3, 3)
     ])
     def test_convergence_creates_NxNxN_calculation(self, a, b, c):
         dims = self.ase_obj.cell * (a, b, c)
