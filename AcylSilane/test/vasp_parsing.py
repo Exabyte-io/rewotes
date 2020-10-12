b37b884f0d16e483e840dba6e3c0103fd783b913
James <None>
2020-10-12 02:21:19 -0400

    Refactor unit tests into separate containers for the VASP I/O and supercell creation.

--- AcylSilane/test/vasp_parsing.py
+++ AcylSilane/test/vasp_parsing.py
@@ -8,16 +8,13 @@ from parameterized import parameterized
 from AcylSilane.convtrack.calculation import Calculation, Convergence
 
 
-class CalculationTests(unittest.TestCase):
+class vasp_interface_tests(unittest.TestCase):
     def setUp(self):
-        self.data_dir = os.path.join(os.path.dirname(os.getcwd()), "data/cu_vasp_files")
+        self.data_dir = os.path.join(os.path.dirname(os.getcwd()), "data/sample_si_calc")
         self.incar = os.path.join(self.data_dir, "INCAR")
         self.poscar = os.path.join(self.data_dir, "POSCAR")
         self.kpoints = os.path.join(self.data_dir, "KPOINTS")
         self.calc = Calculation(self.incar, self.poscar, dims=(1, 1, 1), kpoints=self.kpoints)
-
-        self.convergence = Convergence(self.incar, self.poscar, max_size=3, kpoints=self.kpoints)
-        self.ase_obj = ase.io.read(self.poscar)
 
     def tearDown(self):
         pass
@@ -26,6 +23,23 @@ class CalculationTests(unittest.TestCase):
         self.assertEqual(self.calc.incar, self.incar)
         self.assertEqual(self.calc.poscar, self.poscar)
         self.assertEqual(self.calc.kpoints, self.kpoints)
+
+
+class supercell_creation_tests(unittest.TestCase):
+    def setUp(self):
+        # Build Calculation object
+        self.data_dir = os.path.join(os.path.dirname(os.getcwd()), "data/cu_vasp_files")
+        self.incar = os.path.join(self.data_dir, "INCAR")
+        self.poscar = os.path.join(self.data_dir, "POSCAR")
+        self.kpoints = os.path.join(self.data_dir, "KPOINTS")
+        self.calc = Calculation(self.incar, self.poscar, dims=(1, 1, 1), kpoints=self.kpoints)
+
+        # Build Convergence object
+        self.convergence = Convergence(self.incar, self.poscar, max_size=3, kpoints=self.kpoints)
+        self.ase_obj = ase.io.read(self.poscar)
+
+    def tearDown(self):
+        pass
 
     def test_convergence_creates_1x1x1_calculation(self):
         self.assertTrue(np.all(self.convergence.calculations["111"].crystal.cell == self.ase_obj.cell))
