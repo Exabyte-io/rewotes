4827024951ec59dd387fbd9b8eb705d85ae04739
James <None>
2020-10-12 00:11:22 -0400

    Create Convergence object to contain Calculations objects, add supercell generation functionality to Calculation constructor, create TDD unit-tests for multiple unit cells.

--- AcylSilane/convtrack/calculation.py
+++ AcylSilane/convtrack/calculation.py
@@ -1,7 +1,34 @@
+import itertools
+import typing
+
+import ase.io, ase.build
+
+
 class Calculation(object):
-    def __init__(self, incar: str, poscar: str, kpoints: str = None):
+    def __init__(self, incar: str, poscar: str, dims: typing.Iterable, kpoints: str = None):
         """
         Object containing methods related to a VASP calculation.
+
+        :param incar: Path to the INCAR
+        :type incar: str
+        :param poscar: Path to the POSCAR
+        :type poscar: str
+        :param dims: An iterable containing the unit cell's A/B/C size, in terms of number of repeating units.
+        :type dims: typing.Iterable
+        :param kpoints: Path to the KPOINTS
+        :type kpoints: str
+        """
+        self.incar = incar
+        self.poscar = poscar
+        self.kpoints = kpoints
+        self.crystal = ase.io.read(self.poscar)*dims
+
+
+
+class Convergence(object):
+    def __init__(self, incar, poscar, max_size, kpoints=None):
+        """
+        Automatically tracks convergence and generates new calculations.
 
         :param incar: Path to the INCAR
         :type incar: str
@@ -13,13 +40,19 @@ class Calculation(object):
         self.incar = incar
         self.poscar = poscar
         self.kpoints = kpoints
+        self.max_size = max_size
 
-def example_function():
-    """
-    This is a test
-    :return:
-    """
-    return None
+        self.calculations = {}
+        cell_sizes = list(itertools.product(range(1, max_size + 1),
+                                            range(1, max_size + 1),
+                                            range(1, max_size + 1)
+                                            )
+                          )
+        for size in cell_sizes:
+            dims = "".join(map(str,size))
+            calculation = Calculation(self.incar, self.poscar, size, self.kpoints)
+            self.calculations[dims] = calculation
+
 
 if __name__ == "__main__":
     pass
--- AcylSilane/test/vasp_parsing.py
+++ AcylSilane/test/vasp_parsing.py
@@ -1,7 +1,11 @@
-import unittest
 import os
+import unittest
 
-from AcylSilane.convtrack.calculation import Calculation
+import ase.io
+import numpy as np
+from parameterized import parameterized
+
+from AcylSilane.convtrack.calculation import Calculation, Convergence
 
 
 class CalculationTests(unittest.TestCase):
@@ -10,7 +14,10 @@ class CalculationTests(unittest.TestCase):
         self.incar = os.path.join(self.data_dir, "INCAR")
         self.poscar = os.path.join(self.data_dir, "POSCAR")
         self.kpoints = os.path.join(self.data_dir, "KPOINTS")
-        self.calc = Calculation(self.incar, self.poscar, kpoints=self.kpoints)
+        self.calc = Calculation(self.incar, self.poscar, dims=(1, 1, 1), kpoints=self.kpoints)
+
+        self.convergence = Convergence(self.incar, self.poscar, max_size=5, kpoints=self.kpoints)
+        self.ase_obj = ase.io.read(self.poscar)
 
     def tearDown(self):
         pass
@@ -19,6 +26,23 @@ class CalculationTests(unittest.TestCase):
         self.assertEqual(self.calc.incar, self.incar)
         self.assertEqual(self.calc.poscar, self.poscar)
         self.assertEqual(self.calc.kpoints, self.kpoints)
+
+    def test_convergence_creates_1x1x1_calculation(self):
+        self.assertTrue(np.all(self.convergence.calculations["111"].crystal.cell == self.ase_obj.cell))
+
+    @parameterized.expand([
+        # Supercell size
+        (2, 2, 2),
+        (1, 1, 2),
+        (1, 2, 1),
+        (2, 1, 1),
+        (5, 5, 5),
+        (4, 4, 4)
+    ])
+    def test_convergence_creates_NxNxN_calculation(self, a, b, c):
+        dims = self.ase_obj.cell * (a, b, c)
+        dict_key = "".join(map(str, (a, b, c)))
+        self.assertTrue(np.all(self.convergence.calculations[dict_key].crystal.cell == dims))
 
 
 if __name__ == "__main__":
