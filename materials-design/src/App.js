a8063ab132139e93ce8d25bbd495f0342df5befb
Bryce-Soghigian <brybrycodingguy@gmail.com>
2021-05-01 23:55:51 -0500

    :tada: Initializing our react app with the bry bry cli

---
+++ materials-design/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Hope you enjoy the brybry cli</h2>
+      <h1>Need a new developer on your team?</h1>
+      <a href="https://www.brycesoghigian.com"><h1>Hire me!</h1></a>
+    </div>
+  );
+}
+
+export default App;
