408c4b6be3c61be88f4a0b7408c8aa001036275a
Rhys Goodall <reag2@cam.ac.uk>
2021-07-24 10:56:58 +0100

    fea: split out si-ge composition exploration from training

--- bandgaps/composition.py
+++ bandgaps/composition.py
@@ -1,81 +1,15 @@
 # Train a composition-based model to predict bandgaps
 import os
-import numpy as np
 import pandas as pd
 from joblib import dump
-
-import matplotlib.pyplot as plt
-
-from pymatgen.core.composition import Composition
 
 from matminer.featurizers import composition as cf
 from matminer.featurizers.base import MultipleFeaturizer
 from matminer.utils.io import load_dataframe_from_json
 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor as skrfr
 
-
-class RandomForestRegressor(skrfr):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def predict(self, X_test, return_std=False):
-        """Predict continuous labels y_pred and uncertainty y_std
-        (unless no_std=True) for X_test.
-
-        Args:
-            X_test (array-like, shape=(n_samples, n_features)): Input data.
-            no_std=False (bool, optional): Don't return y_std if set to true.
-
-        Returns:
-            2- or 1-tuple: y_pred (and y_std)
-        """
-        if self.criterion != "mse":
-            err = f"Expected impurity to be 'mse', instead got {self.criterion}."
-            raise ValueError(err)
-
-        y_pred = super().predict(X_test)
-        if return_std:
-            y_std = self._return_std(X_test, self.estimators_, y_pred)
-            return y_pred, y_std
-        else:
-            return y_pred
-
-
-    @staticmethod
-    def _return_std(X_test, trees, predictions):
-        """Returns `std(Y | X_test)` calculated via E[Var(Y | Tree)] + Var(E[Y | Tree])
-        where P(Tree) is `1 / len(trees)`.
-
-        Note: Another option for estimating confidence intervals would be prediction
-        variability, i.e. how influential training set is for producing observed random
-        forest predictions. E.g. https://github.com/scikit-learn-contrib/forest-confidence-interval.
-        Empirically the below method to obtain y_std was found to be noticeably more accurate.
-
-        Args:
-            X_test (array-like, shape=(n_samples, n_features)): Input data.
-            trees (list, shape=(n_estimators,)): List of fitted sklearn trees as obtained
-                from the estimators_ attribute of a fitted RandomForestRegressor.
-            predictions (array-like, shape=(n_samples,)): Prediction for each sample
-                as returned by RandomForestRegressor.
-
-        Returns:
-            array-like, shape=(n_samples,): Standard deviation of y_pred at X_test. If criterion
-            is set to "mse", then std[i] ~= std(y | X_test[i]).
-        """
-        y_var = np.zeros(len(X_test))
-
-        # This derives std(y | X_test) as described in sec. 4.3.2 of
-        # http://arxiv.org/pdf/1211.0906v2.pdf
-        for tree in trees:
-            var_tree = tree.tree_.impurity[tree.apply(X_test)]
-            mean_tree = tree.predict(X_test)
-            y_var += var_tree + mean_tree ** 2
-
-        y_var /= len(trees)
-        y_var -= predictions ** 2
-        return y_var ** 0.5
+from utils import RandomForestRegressor
 
 
 DATA_SEED = 42
@@ -155,38 +89,4 @@ print("Train Random Forest Regressor")
 model.fit(df_train[feature_labels].values, df_train["band_gap"].values)
 
 # Save the model
-dump(model, PATH+f"/models/composition-rfr-{data}.pkl")
-
-print("Evaluate results on Si-Ge chemical System")
-df_test_comps = pd.DataFrame()
-df_test_comps["composition"] = [Composition({"Si": 1-x, "Ge": x}) for x in np.linspace(0, 1)]
-df_test_comps = comp_features.featurize_dataframe(df_test_comps, col_id="composition")
-
-# y_pred = model.predict(df_test_comps[feature_labels])
-y_pred, y_unc = model.predict(df_test_comps[feature_labels], return_std=True)
-
-plt.figure(figsize=(8, 8))
-plt.xlim
-plt.plot(np.linspace(0, 1), y_pred, label="RFR Prediction")
-plt.fill_between(np.linspace(0, 1), y_pred+y_unc, y_pred-y_unc, label="RFR Prediction", alpha=0.4)
-
-# https://journals.aps.org/prb/abstract/10.1103/PhysRevB.40.5683
-fitted = [1.155 - 0.43*x + 0.206*x**2 if x < 0.85 else 2.010 - 1.270*x for x in np.linspace(0, 1)]
-
-plt.plot(np.linspace(0, 1), fitted, label="Experimental Fit")
-ax = plt.gca()
-ax.set_xlim(0, 1)
-ax.set_ylim(0, 1.5)
-
-# plt.scatter((0, 0.125, 0.25, 0.5, 0.75, 1), (0.8527, 0.8585, 0.0000, 0.4267, 0.0000, 0.0000), color="red", marker="x", clip_on=False, zorder=3, label="Training Data")
-# plt.scatter((0, 0.125, 0.5), (0.8527, 0.8585, 0.4267), color="red", marker="x", clip_on=False, zorder=3, label="Training Data")
-
-plt.legend(frameon=False)
-
-xlabel = r"$x$" + " in " + r"$Si_{x}Ge_{1-x}$"
-ax.set_xlabel(xlabel)
-
-ylabel = r"$E_g$" + " / eV"
-ax.set_ylabel(ylabel)
-plt.savefig(PATH+f"/SiGe-composition-{data}.pdf")
-plt.show()
+dump(model, PATH+f"/models/composition-rfr-{data}.pkl.gz", )
---
+++ bandgaps/explore.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+import pandas as pd
+from joblib import load
+
+import matplotlib.pyplot as plt
+
+from pymatgen.core.composition import Composition
+
+from matminer.featurizers import composition as cf
+from matminer.featurizers.base import MultipleFeaturizer
+
+from utils import RandomForestRegressor
+
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+# data = "non-metals"
+data = "combined"
+
+model = load(PATH+f"/models/composition-rfr-{data}.pkl.gz")
+
+comp_features = MultipleFeaturizer([
+    cf.Stoichiometry(),
+    cf.ElementProperty.from_preset("magpie"),
+    cf.ValenceOrbital(props=["avg"]),
+    cf.IonProperty(fast=True)
+])
+
+feature_labels = comp_features.feature_labels()
+
+print("Evaluate results on Si-Ge chemical System")
+df_test_comps = pd.DataFrame()
+df_test_comps["composition"] = [Composition({"Si": 1-x, "Ge": x}) for x in np.linspace(0, 1)]
+df_test_comps = comp_features.featurize_dataframe(df_test_comps, col_id="composition")
+
+# y_pred = model.predict(df_test_comps[feature_labels])
+y_pred, y_unc = model.predict(df_test_comps[feature_labels], return_std=True)
+
+plt.figure(figsize=(8, 8))
+plt.xlim
+plt.plot(np.linspace(0, 1), y_pred, label="RFR Prediction")
+plt.fill_between(np.linspace(0, 1), y_pred+y_unc, y_pred-y_unc, label="RFR Prediction", alpha=0.4)
+
+# https://journals.aps.org/prb/abstract/10.1103/PhysRevB.40.5683
+fitted = [1.155 - 0.43*x + 0.206*x**2 if x < 0.85 else 2.010 - 1.270*x for x in np.linspace(0, 1)]
+
+plt.plot(np.linspace(0, 1), fitted, label="Experimental Fit")
+ax = plt.gca()
+ax.set_xlim(0, 1)
+ax.set_ylim(0, 1.5)
+
+# plt.scatter((0, 0.125, 0.25, 0.5, 0.75, 1), (0.8527, 0.8585, 0.0000, 0.4267, 0.0000, 0.0000), color="red", marker="x", clip_on=False, zorder=3, label="Training Data")
+# plt.scatter((0, 0.125, 0.5), (0.8527, 0.8585, 0.4267), color="red", marker="x", clip_on=False, zorder=3, label="Training Data")
+
+plt.legend(frameon=False)
+
+xlabel = r"$x$" + " in " + r"$Si_{x}Ge_{1-x}$"
+ax.set_xlabel(xlabel)
+
+ylabel = r"$E_g$" + " / eV"
+ax.set_ylabel(ylabel)
+plt.savefig(PATH+f"/SiGe-composition-{data}.pdf")
+plt.show()
