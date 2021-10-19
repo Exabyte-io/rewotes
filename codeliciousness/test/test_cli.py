0cd577520278ac5377ebc6c4487d211cc91ba87d
codeliciousness <exabytecandidate@gmail.com>
2021-10-18 22:52:30 -0400

    test: cli tests

---
+++ codeliciousness/test/test_cli.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from basistron import cli
+from basistron.model import Driver
+
+command = [
+    "--target_property",
+    "energy_convergence",
+    "--reference_value",
+    "-100",
+    "--xyz_path",
+    "/path/to/file",
+]
+
+
+def mock_file(tmppath, h2):
+    path = (tmppath / "h2.xyz").as_posix()
+    with open(path, "w") as f:
+        f.write(h2)
+    return path
+
+
+@pytest.mark.parametrize(
+    "input_data, expected, raises",
+    [
+        ([], None, True),
+        (
+            ["--xyz_path", "/path/to/file"],
+            None,
+            True,
+        ),
+        (
+            [
+                "--xyz_path",
+                "/path/to/file",
+                "--target_property",
+                "energy_convergence",
+            ],
+            None,
+            True,
+        ),
+        (
+            command,
+            {
+                "xyz_path": "/path/to/file",
+                "target_property": "energy_convergence",
+                "reference_value": -100.0,
+            },
+            False,
+        ),
+    ],
+)
+def test_get_parser(input_data, expected, raises):
+    parser = cli.get_parser()
+    if raises:
+        with pytest.raises(SystemExit):
+            parser.parse_args(input_data)
+    else:
+        args = parser.parse_args(input_data)
+        for key, val in expected.items():
+            assert getattr(args, key) == val
+
+
+def test_process_args(tmppath, h2, h2dat):
+    path = mock_file(tmppath, h2)
+    parser = cli.get_parser()
+    args = parser.parse_args(command[:-1] + [path])
+    ret = cli.process_args(args)
+    assert isinstance(ret, Driver)
+    assert ret.xyz_data == h2dat
+
+
+def test_process_args_fail(tmppath, h2, h2dat):
+    path = mock_file(tmppath, h2)
+    parser = cli.get_parser()
+    cmd = command.copy()
+    cmd[1] = "not_recognized"
+    args = parser.parse_args(cmd[:-1] + [path])
+    with pytest.raises(Exception):
+        cli.process_args(args)
