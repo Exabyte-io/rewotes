9c161587e8a64a9e26ce2d5e459dcd173e28b859
Jonathon Vandezande <jevandezande@gmail.com>
2022-02-19 14:47:35 -0500

    Adds additional tests.

--- jevandezande/tests/test_basis_set_selector.py
+++ jevandezande/tests/test_basis_set_selector.py
@@ -8,12 +8,23 @@ def H2():
     return run("H 0 0 0\nH 0 0 1")
 
 
-def test_run():
-    mf = run("H 0 0 0\nH 0 0 1")
-    assert mf.e_tot == approx(-1.12761841620146)
+@fixture
+def H2O():
+    return run("H\nO 1 1\nH 2 1 1 105")
 
 
-@mark.xfail
-def test_get_homo_lumo_gap(H2):
-    get_homo_lumo_gap(H2)
-    assert 0
+@mark.parametrize(
+    "mol, energy",
+    [
+        ["He", -2.89812055849662],
+        ["H 0 0 0\nH 0 0 1", -1.150637241322931],
+        ["H 0 0 0\nO 0 0 1\nH 0 1 1", -76.35496969473999],
+    ],
+)
+def test_run(mol, energy):
+    assert run(mol).e_tot == approx(energy)
+
+
+def test_get_homo_lumo_gap(H2, H2O):
+    assert get_homo_lumo_gap(H2) == approx(-0.33167556339619)
+    assert get_homo_lumo_gap(H2O) == approx(-0.2528650709452739)
