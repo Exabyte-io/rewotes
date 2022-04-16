23b829b6bc76ae8a9fb66a2c27fdde8adc99a87b
Vladislav Laktionov <vladislav.laktionov98@gmail.com>
2022-04-16 13:33:32 +0300

    feat: added dependencies(reactflow, mui), styled root div

--- Vinkersa/src/App.tsx
+++ Vinkersa/src/App.tsx
@@ -1,44 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Box } from "@mui/material";
+import React from "react";
+import FlowChart from "./pages/FlowChart";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+      <Box sx={{width: '100%', height: '100%'}}>
+        <FlowChart/>
+      </Box>
   )
 }
 
---
+++ Vinkersa/src/pages/FlowChart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+
+const FlowChart: React.FC = () => {
+
+
+    return <></>
+}
+
+export default FlowChart
