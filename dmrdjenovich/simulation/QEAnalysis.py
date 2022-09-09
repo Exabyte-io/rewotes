2c72a8593064460a22d4e1ecaa96deca056c4f4f
David Mrdjenovich <dmrdjenovich@berkeley.edu>
2022-09-09 16:20:30 -0700

    fleshing out an initial implementation

--- dmrdjenovich/simulation/QEAnalysis.py
+++ dmrdjenovich/simulation/QEAnalysis.py
@@ -1,9 +1,20 @@
-# File containing analysis tools that can be employed to analyze
-# the raw output of a QuantumEspresso run.
+import xml.etree.ElementTree as ET
 
-def get_final_energy(output)
+class QEAnalysis:
     """
-    Given the output file of a QE run, returns the final energy
+    Class containing standard methods for processing the output
+    of a Quantum Espresso simulation.
     """
-    pass
+    
+    def __init__(self, src):
+        self.root = ET.parse(src).getroot()
+        
+    def get_energy(self):
+        """
+        Returns an array of energies, each corresponding to an
+        ionic relaxation step in order of initial to final
+        geometry.
+        """
+        raw = self.root.findAll("step/total_energy/etot")
+        return [float(x.text) for x in raw]
 
--- dmrdjenovich/simulation/QEProcess.py
+++ dmrdjenovich/simulation/QEProcess.py
@@ -1,4 +1,6 @@
-from computing import Executable
+from computing import Executable, Resources
+import xml.etree.ElementTree as ET
+import os
 
 class QEProcess(Executable):
     """
@@ -18,8 +20,55 @@ class QEProcess(Executable):
     from QEAnalysis.py for custom results parsing.
     """
 
-    def get_resources():
-        pass
+    req_resources = Resources(0, 1)
+
+    def __init__(self, dir, spec):
+        self.dir = dir
+        self.spec = spec
+        self.error_state = QEError.NO_ERROR
+        self.stop_flag = False
+        self.return_flag = False
+
+    def get_dir(self):
+        return self.dir
+        
+    def get_spec(self):
+        return self.spec
+
+    def get_resources(self):
+        return QEProcess.req_resources
         
     def exec(envr):
-        pass
+        if not os.path.isdir(self.dir):
+            self.handle_error(QEError.SIM_DIR_DOES_NOT_EXIST)
+        if self.stop_flag:
+            return self.return_flag
+        
+        f_target = os.join(self.dir,
+                        self.spec.dict["&CONTROL"].get("prefix", "pwscf") + .xml)
+        if not os.path.exists(f_target):
+            self.handle_error(QEError.UNABLE_TO_READ_OUTPUT)
+        if self.stop_flag:
+            return self.return_flag
+            
+        QEAnalysis analysis = QEAnalysis(f_target)
+        get_results(analysis)
+        
+        self.error_state = QEError.NO_ERROR
+        return True
+        
+    def get_results(self, analysis):
+        """
+        Method stub meant to be overridden for custom analysis steps.
+        """
+        return
+        
+    def handle_error(self, error):
+        self.error_state = error
+        if error == QEError.SIM_DIR_DOES_NOT_EXIST:
+            print("Fatal Error: Sim Directory not found.")
+            print(self.dir)
+            self.stop_flag = True
+            self.return_flag = False
+            return
+            
