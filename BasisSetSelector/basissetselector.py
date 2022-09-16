a62cb6423dd6c78dc6546e78e8e19ca987e2a863
Samarth Sandeep <gravityshouldbenaut>
2022-09-15 18:44:09 -0700

    multi-atom systems setup using addition of hartree energies

--- BasisSetSelector/basissetselector.py
+++ BasisSetSelector/basissetselector.py
@@ -1,22 +1,35 @@
 import pandas as pd  
+from rdkit.Chem import AllChem as Chem
+
 
 def find_precision(theoretical, experimental):
 	return abs((theoretical-experimental)/experimental)
 
-def find_optimal_basis_sets(atoms, precisionInPercent):
+def find_optimal_basis_sets_SMILES(smile, precisionInPercent):
 	bestBasisSets = []
 	precisionDecimal = precisionInPercent / 100
 	groundStateDF = pd.read_csv("groundstateenergies.csv",index_col=0)
-	groundStateEnergyForAtom = groundStateDF["Experimental Ground State Energy from AE17 UMN (Hartree)"][atoms]
-	if(find_precision(groundStateDF["cc-pVTZ + B3LYP"][atoms],groundStateEnergyForAtom) <= precisionDecimal):
+	mol = Chem.MolFromSmiles(smile)
+	arrayOfAtomsInSmile = [atom.GetSymbol() for atom in mol.GetAtoms()]
+	groundStateSum = 0
+	ccpVTZb3lypSum = 0
+	augccpDZb3lypSum = 0
+	sixthreeoneb3lypsum = 0
+	for atom in arrayOfAtomsInSmile:
+		groundStateSum += groundStateDF["Experimental Ground State Energy from AE17 UMN (Hartree)"][atom]	
+		ccpVTZb3lypSum += groundStateDF["cc-pVTZ + B3LYP"][atom]
+		sixthreeoneb3lypsum += groundStateDF["6-31G + B3LYP"][atom]
+		augccpDZb3lypSum += groundStateDF["aug-cc-pVDZ + B3LYP"][atom]
+	print(sixthreeoneb3lypsum)
+	if(find_precision(ccpVTZb3lypSum,groundStateSum) <= precisionDecimal):
 		bestBasisSets.append("cc-pVTZ + B3LYP")
-	if(find_precision(groundStateDF["6-31G + B3LYP"][atoms],groundStateEnergyForAtom) <= precisionDecimal):
+	if(find_precision(sixthreeoneb3lypsum,groundStateSum) <= precisionDecimal):
 		bestBasisSets.append("6-31G + B3LYP")
-	if(find_precision(groundStateDF["aug-cc-pVDZ + B3LYP"][atoms],groundStateEnergyForAtom) <= precisionDecimal):
+	if(find_precision(augccpDZb3lypSum,groundStateSum) <= precisionDecimal):
 		bestBasisSets.append("aug-cc-pVDZ + B3LYP")
-	if((find_precision(groundStateDF["aug-cc-pVDZ + B3LYP"][atoms],groundStateEnergyForAtom) > precisionDecimal) and (find_precision(groundStateDF["cc-pVTZ + B3LYP"][atoms],groundStateEnergyForAtom) > precisionDecimal) and (find_precision(groundStateDF["6-31G + B3LYP"][atoms],groundStateEnergyForAtom) > precisionDecimal)):
+	if((find_precision(ccpVTZb3lypSum,groundStateSum) > precisionDecimal) and (find_precision(sixthreeoneb3lypsum,groundStateSum) > precisionDecimal) and (find_precision(augccpDZb3lypSum,groundStateSum) > precisionDecimal)):
 		bestBasisSets.append("cc-pVTZ + B3LYP,6-31G + B3LYP, and aug-cc-pVDZ + B3LYP are too imprecise. Please try another basis set")
 
 	return bestBasisSets
 
-print(find_optimal_basis_sets("Li", 0.14))
+print(find_optimal_basis_sets_SMILES("CC", 0.01))
