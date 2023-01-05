40fd6889444978de84da116178e84d3a18e556f7
Aaron Stanek <amr.stanek@gmail.com>
2023-01-04 16:36:55 -0800

    simplified Material class

--- aaronstanek/pkg/material/Material.py
+++ aaronstanek/pkg/material/Material.py
@@ -1,62 +1,32 @@
-from ..elements import element_lookup_table
 from .material_pb2 import Material as protobuf_material
 
 class Material(object):
     def __init__(self, initializer = None):
+        self.__dict__["_values"] = protobuf_material()
         if initializer is None:
-            self._value = protobuf_material()
+            pass
         elif type(initializer) == bytes:
-            self._value = protobuf_material()
-            self._value.ParseFromString(initializer)
+            self.__dict__["_values"] .ParseFromString(initializer)
         else:
             raise TypeError("Expected bytes. Found: " + str(type(initializer)))
+    def __getattr__(self, property_name):
+        return getattr(self.__dict__["_values"] , property_name)
+    def __setattr__(self, property_name, property_value):
+        return setattr(self.__dict__["_values"] , property_name, property_value)
+    def __delattr__(self, property_name):
+        return delattr(self.__dict__["_values"] , property_name)
     def serialize(self):
-        return self._value.SerializeToString()
-    def _get_material_id(self):
-        return self._value.material_id
-    def _set_material_id(self, value):
-        if value is None:
-            self._value.material_id = ""
+        return self.__dict__["_values"] .SerializeToString()
+    def save_to_file(self, filename):
+        if type(filename) != str:
+            raise TypeError("Expected str. Found: " + str(type(filename)))
         else:
-            self._value.material_id = value
-    def _del_material_id(self):
-        self._set_material_id(None)
-    material_id = property(_get_material_id, _set_material_id, _del_material_id)
-    def _get_optional_double(self, property_name):
-        optional_double = getattr(self._value, property_name)
-        if optional_double.is_defined:
-            return optional_double.value
+            with open(filename, "wb") as file:
+                file.write(self.serialize())
+    @staticmethod
+    def load_from_file(filename):
+        if type(filename) != str:
+            raise TypeError("Expected str. Found: " + str(type(filename)))
         else:
-            return None
-    def _set_optional_double(self, property_name, value):
-        optional_double = getattr(self._value, property_name)
-        if value is None:
-            optional_double.value = 0
-            optional_double.is_defined = False
-        else:
-            optional_double.value = value
-            optional_double.is_defined = True
-    def _del_optional_double(self, property_name):
-        self._set_optional_double(property_name, None)
-
-def apply_optional_double_properties():
-    for property_name in ["band_gap"]:
-        setattr(Material, property_name, property(
-            lambda self: self._get_optional_double(property_name),
-            lambda self, value: self._set_optional_double(property_name, value),
-            lambda self: self._del_optional_double(property_name)
-        ))
-
-apply_optional_double_properties()
-
-def apply_atoms_of_element_properties():
-    for atomic_number in range(1,119):
-        internal_name = "atoms_of_element_" + str(atomic_number)
-        external_name = "atoms_of_" + element_lookup_table.lookup_by_atomic_number(atomic_number).symbol
-        setattr(Material, external_name ,property(
-            lambda self: getattr(self._value, internal_name),
-            lambda self, value: setattr(self._value, internal_name, value),
-            lambda self: setattr(self._value, internal_name, 0)
-        ))
-
-apply_atoms_of_element_properties()
+            with open(filename, "rb") as file:
+                return Material(file.read())
