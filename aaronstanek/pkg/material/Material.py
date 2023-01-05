fbf5f08b31b462049f35030bbbee3bb21b8d9a77
Aaron Stanek <amr.stanek@gmail.com>
2023-01-04 18:05:10 -0800

    add composition encoder

--- aaronstanek/pkg/material/Material.py
+++ aaronstanek/pkg/material/Material.py
@@ -1,4 +1,5 @@
 from .material_pb2 import Material as protobuf_material
+from .composition_encoder import encode_composition, decode_composition
 
 class Material(object):
     def __init__(self, initializer = None):
@@ -10,9 +11,19 @@ class Material(object):
         else:
             raise TypeError("Expected bytes. Found: " + str(type(initializer)))
     def __getattr__(self, property_name):
-        return getattr(self.__dict__["_values"] , property_name)
+        if property_name == "composition":
+            return decode_composition(self.__dict__["_values"].composition)
+        elif property_name == "composition_reduced":
+            return decode_composition(self.__dict__["_values"].composition_reduced)
+        else:
+            return getattr(self.__dict__["_values"] , property_name)
     def __setattr__(self, property_name, property_value):
-        return setattr(self.__dict__["_values"] , property_name, property_value)
+        if property_name == "composition":
+            return setattr(self.__dict__["_values"], property_name, encode_composition(property_value))
+        elif property_name == "composition_reduced":
+            return setattr(self.__dict__["_values"], property_name, encode_composition(property_value))
+        else:
+            return setattr(self.__dict__["_values"] , property_name, property_value)
     def __delattr__(self, property_name):
         return delattr(self.__dict__["_values"] , property_name)
     def serialize(self):
@@ -30,3 +41,31 @@ class Material(object):
         else:
             with open(filename, "rb") as file:
                 return Material(file.read())
+    # def decode_composition(self):
+    #     return decode_composition(self.composition)
+    # def encode_composition(self, value):
+    #     self.composition = encode_composition(value)
+    # def decode_composition_reduced(self):
+    #     return decode_composition(self.composition_reduced)
+    # def encode_composition_reduced(self, value):
+    #     self.composition_reduced = encode_composition(value)
+    # composition = property(
+    #     lambda self: decode_composition(getattr(self.__dict__["_values"], "composition")),
+    #     lambda self, value: setattr(self.__dict__["_values"], "composition", encode_composition(value)),
+    #     lambda self: setattr(self.__dict__["_values"], "composition", b'')
+    # )
+    # composition_reduced = property(
+    #     lambda self: decode_composition(getattr(self.__dict__["_values"], "composition_reduced")),
+    #     lambda self, value: setattr(self.__dict__["_values"], "composition_reduced", encode_composition(value)),
+    #     lambda self: setattr(self.__dict__["_values"], "composition_reduced", b'')
+    # )
+
+# def add_composition_properties():
+#     for property_name in ["composition", "composition_reduced"]:
+#         setattr(Material, property_name, property(
+#             lambda self: decode_composition(getattr(self.__dict__["_values"], property_name)),
+#             lambda self, value: setattr(self.__dict__["_values"], property_name, encode_composition(value)),
+#             lambda self: setattr(self.__dict__["_values"], property_name, b''),
+#         ))
+
+# add_composition_properties()
