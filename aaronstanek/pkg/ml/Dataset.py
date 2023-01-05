a5a870832c9e0b3a969565ef1541454e210731d7
Aaron Stanek <amr.stanek@gmail.com>
2023-01-05 13:17:57 -0800

    add Dataset

---
+++ aaronstanek/pkg/ml/Dataset.py
@@ -0,0 +1,33 @@
+import hashlib
+import numpy
+import torch
+from ..material import MaterialArchive
+from .DataRangeEncoderArray import DataRangeEncoderArray
+
+# int -> bool
+# pure function which returns True for 20% of input values
+def assign_as_testing(index):
+    result = hashlib.md5(str(index).encode("utf-8")).digest()
+    return result[0] < 241 and result[1] < 119 and result[2] < 117
+
+def separate_y_x(data):
+    y = torch.from_numpy(data[:,0])
+    x = torch.from_numpy(data[:,1:])
+    return y, x
+
+class Dataset(object):
+    def __init__(self, archive):
+        if not isinstance(archive, MaterialArchive):
+            raise TypeError("Expected instance of MaterialArchive. Found: " + str(type(archive)))
+        numpy_double_array = archive.to_numpy_double_array()
+        self.data_range_encoder_array = DataRangeEncoderArray(numpy_double_array)
+        training_data = []
+        testing_data = []
+        for material_index in range(len(numpy_double_array)):
+            material = self.data_range_encoder_array.encode(numpy_double_array[material_index])
+            [training_data,testing_data][assign_as_testing(material_index)].append(material)
+        self.training_y, self.training_x = separate_y_x(numpy.array(training_data))
+        self.testing_y, self.testing_x = separate_y_x(numpy.array(testing_data))
+    @staticmethod
+    def load_from_archive_file(filename):
+        return Dataset(MaterialArchive.load_from_file(filename))
--- aaronstanek/pkg/ml/__init__.py
+++ aaronstanek/pkg/ml/__init__.py
@@ -1,2 +1,3 @@
 from .DataRangeEncoder import DataRangeEncoder
 from .DataRangeEncoderArray import DataRangeEncoderArray
+from .Dataset import Dataset
