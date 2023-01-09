2b919ea27564d2af7df5f8df109d92130b4150f4
Aaron Stanek <amr.stanek@gmail.com>
2023-01-08 17:48:39 -0800

    TrainingDataManager docstring

--- aaronstanek/pkg/ml/TrainingDataManager.py
+++ aaronstanek/pkg/ml/TrainingDataManager.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import hashlib
 import numpy
 import torch
 from ..material import MaterialArchive
@@ -7,17 +6,15 @@ from .NormalizationEncoderArray import NormalizationEncoderArray
 from .Dataset import Dataset
 from typing import Union
 
-# int -> bool
-# pure function which returns True for 20% of input values
-
-
-def assign_as_testing(index: int) -> bool:
-    result = hashlib.md5(str(index).encode('utf-8')).digest()
-    return result[0] < 241 and result[1] < 119 and result[2] < 117
-
 
 class TrainingDataManager(object):
+    '''
+    Builds and holds training and testing Dataset objects.
+    '''
     def __init__(self, initializer: Union[MaterialArchive, numpy.ndarray], batch_size: int = 64):
+        '''
+        Create a TrainingDataManager from either a MaterialArchive or a numpy array.
+        '''
         try:
             batch_size = int(batch_size)
         except:
@@ -44,11 +41,11 @@ class TrainingDataManager(object):
         self.total_feature_width = len(self.data_range_encoder_array)
         training_data = []
         testing_data = []
+        rng = numpy.random.Generator(numpy.random.MT19937(123456))
         for material_index in range(len(numpy_double_array)):
             material = self.data_range_encoder_array.encode(
                 numpy_double_array[material_index])
-            [training_data, testing_data][assign_as_testing(
-                material_index)].append(material)
+            [training_data, testing_data][rng.integers(0,5) == 0].append(material)
         self.training = torch.utils.data.DataLoader(
             Dataset(training_data), batch_size=batch_size, shuffle=True)
         self.testing = torch.utils.data.DataLoader(
@@ -56,8 +53,14 @@ class TrainingDataManager(object):
 
     @staticmethod
     def load_from_archive_file(filename: str) -> TrainingDataManager:
+        '''
+        Create a TrainingDataManager using a file generated from MaterialArchive.save_to_file.
+        '''
         return TrainingDataManager(MaterialArchive.load_from_file(filename))
 
     @staticmethod
     def load_from_numpy_file(filename: str) -> TrainingDataManager:
+        '''
+        Create a TrainingDataManager using a file generated from MaterialArchive.save_as_numpy.
+        '''
         return TrainingDataManager(numpy.load(filename))
