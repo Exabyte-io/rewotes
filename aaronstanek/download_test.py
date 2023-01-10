fb48ad045d188f231e4132a8a75dfe23d0b03349
Aaron Stanek <amr.stanek@gmail.com>
2023-01-09 20:55:47 -0800

    add requirements.txt

--- aaronstanek/download_test.py
+++ aaronstanek/download_test.py
@@ -3,8 +3,6 @@ from pkg import Downloader
 with open('api_key.txt', 'r', encoding='utf-8') as file:
     api_key = file.read()
 
-dwn = Downloader('materials.materialarchive', api_key)
+dwn = Downloader(api_key)
 
-elapsed = dwn.download()
-
-print('Time Elapsed:', elapsed)
+dwn.download_to_file('materials.materialarchive')
