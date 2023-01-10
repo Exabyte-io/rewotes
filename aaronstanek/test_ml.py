4de19db807c0577a697fe553b78eebfb108a01c9
Aaron Stanek <amr.stanek@gmail.com>
2023-01-09 22:56:55 -0800

    test_ml no longer needs to declare model as double

--- aaronstanek/test_ml.py
+++ aaronstanek/test_ml.py
@@ -4,8 +4,6 @@ a = pkg.TrainingDataManager.load_from_numpy_file('materials.npy')
 
 m = pkg.Model(a)
 
-m.double()
-
 print(m.test_standard_deviation(a))
 print(m.test_with_error(a, iteration_count=50))
 
