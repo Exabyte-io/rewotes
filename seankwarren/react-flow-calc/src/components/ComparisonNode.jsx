ab492498bf61177ff21fc82d190c25e445451b45
seankwarren <seankwarren722@gmail.com>
2023-04-08 14:07:44 -0400

    feat: binary comparison node

--- seankwarren/react-flow-calc/src/components/ComparisonNode.jsx
+++ seankwarren/react-flow-calc/src/components/ComparisonNode.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Handle } from 'reactflow';
 
-const ComparisonNode = () => {
-  return (
-    <div className='node comparison round'>ComparisonNode</div>
-  )
-}
+export const ComparisonNode = ({ id, data, updateOutputNodes }) => {
+    const handleSelectChange = (e) => {
+        data.onChange(e.target.value, updateOutputNodes);
+        updateOutputNodes();
+    };
 
-export default ComparisonNode
+    return (
+        <div className='node comparison round' data-testid='comparison-node'>
+            <Handle
+                className='handle input'
+                type='target'
+                position='left'
+                id={`${id}-left-top`}
+                style={{
+                    top: '30%',
+                }}
+            />
+            <Handle
+                className='handle input'
+                type='target'
+                position='left'
+                id={`${id}-left-bottom`}
+                style={{
+                    top: '70%',
+                }}
+            />
+            <select onChange={handleSelectChange}>
+                <option value='greater'>{'>'}</option>
+                <option value='less'>{'<'}</option>
+                <option value='greaterEqual'>{'>='}</option>
+                <option value='lessEqual'>{'<='}</option>
+                <option value='equal'>{'=='}</option>
+                <option value='notEqual'>{'!='}</option>
+            </select>
+            <Handle
+                className='handle output'
+                type='source'
+                position='right'
+                id={`${id}-right`}
+            />
+        </div>
+    );
+};
+
+export default ComparisonNode;
