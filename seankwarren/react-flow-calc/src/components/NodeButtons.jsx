6672832c089e89388187e2dcf98517e39d10ee06
seankwarren <seankwarren722@gmail.com>
2023-04-08 00:30:41 -0400

    feat: font changed and css modularized

--- seankwarren/react-flow-calc/src/components/NodeButtons.jsx
+++ seankwarren/react-flow-calc/src/components/NodeButtons.jsx
@@ -4,28 +4,28 @@ const NodeButtons = ({ addNode, handleDragStart }) => {
     return (
         <div className='buttons-panel'>
             <button
-                className='node-button input'
+                className='node button input'
                 draggable='true'
                 onDragStart={(e) => handleDragStart(e, 'input')}
             >
                 in
             </button>
             <button
-                className='node-button operation round'
+                className='node button operation round'
                 draggable='true'
                 onDragStart={(e) => handleDragStart(e, 'operation')}
             >
                 +
             </button>
             <button
-                className='node-button comparison round'
+                className='node button comparison round'
                 draggable='true'
                 onDragStart={(e) => handleDragStart(e, 'comparison')}
             >
                 &gt;
             </button>
             <button
-                className='node-button output'
+                className='node button output'
                 draggable='true'
                 onDragStart={(e) => handleDragStart(e, 'output')}
             >
