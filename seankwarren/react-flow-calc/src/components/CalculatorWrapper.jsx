da840fd8f23cf0ec782524fdcf649a3961b84bca
seankwarren <seankwarren722@gmail.com>
2023-04-09 15:25:05 -0400

    fix(style): init instructions restyled

--- seankwarren/react-flow-calc/src/components/CalculatorWrapper.jsx
+++ seankwarren/react-flow-calc/src/components/CalculatorWrapper.jsx
@@ -17,11 +17,11 @@ const CalculatorWrapper = () => {
             type: 'default',
             position: { x: 250, y: 150 },
             selected: true,
-            data: { label: `
-            1. Drag and drop a node onto the flowchart\n
-            2. Connect nodes using the handles on each node. Ensure to connect an outputs (black) to input (white).\n
-            3. Edit input values and select functions from the dropdown to see the updated results in the output nodes.\n
-            4. View the JSON representation of nodes and edges on the right.` },
+            data: { label: 
+            `1. Drag and drop a node onto the flowchart\n` +
+            `2. Connect nodes using the handles on each node. Ensure to connect an outputs (black) to input (white).\n` +
+            `3. Edit input values and select functions from the dropdown to see the updated results in the output nodes.\n` +
+            `4. View the JSON representation of nodes and edges on the right.` },
         },
     ]);
     const [edges, setEdges] = useState([]);
