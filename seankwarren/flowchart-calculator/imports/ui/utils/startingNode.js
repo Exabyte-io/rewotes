0377522e05326e10e3816d32c84430faa891900f
seankwarren <seankwarren722@gmail.com>
2023-04-10 16:27:24 -0400

    fix: instruction typo corrections

--- seankwarren/flowchart-calculator/imports/ui/utils/startingNode.js
+++ seankwarren/flowchart-calculator/imports/ui/utils/startingNode.js
@@ -4,9 +4,9 @@ const startingNode = {
     position: { x: 100, y: 150 },
     selected: true,
     data: { label: 
-    `1. Drag and drop a node onto the flowchart\n` +
-    `2. Connect nodes using the handles on each node. Ensure to connect an outputs (black) to input (white).\n` +
-    `3. Edit input values and select functions from the dropdown to see the updated results in the output nodes.\n` +
+    `1. Drag and drop nodes onto the flowchart\n` +
+    `2. Connect nodes using the handles on each node. Ensure to connect outputs (black) to inputs (white).\n` +
+    `3. Edit input values and select functions to see the updated outputs.\n` +
     `4. View the JSON representation of nodes and edges on the right.` },
 }
 
