8f4f2a43ed87105b120d7b3ad1479478ebabf6e7
Serge Kurenkov <serge@filefinder.io>
2023-04-13 14:59:41 -0700

    terraform apply/destroy from ansible flow

--- itgold/scripts/provision.sh
+++ itgold/scripts/provision.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo 'Provisioning environment: dev'
-ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/provision_host.yml $1 $2 $3 $4
+ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/terraform-apply.yml $1 $2 $3 $4
+
+ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/terraform-destroy.yml $1 $2 $3 $4
---
+++ itgold/workspace/README.md
@@ -0,0 +1,2 @@
+# Overview
+This is the working folder for projects that is going to be checked out by the pipeline
