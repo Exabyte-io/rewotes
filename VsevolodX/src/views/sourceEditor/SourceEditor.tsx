30989eebcc4d4d5a1631c11d2630e74e9f7be781
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-04-15 14:13:11 -0400

    Fix: corrected checking for XYZ validity

--- VsevolodX/src/views/sourceEditor/SourceEditor.tsx
+++ VsevolodX/src/views/sourceEditor/SourceEditor.tsx
@@ -1,8 +1,8 @@
 import { Card, ControlGroup, TextArea, InputGroup, Tag, Button } from '@blueprintjs/core'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './SourceEditor.module.scss';
 import { useSourceContext } from '../../context/SourceContext';
-import { useAtomsContext, Atom} from '../../context/AtomsContext';
+import { useAtomsContext, Atom } from '../../context/AtomsContext';
 import VStack from '../../components/utils/VStack';
 import ViewHeading from '../../components/view_heading/ViewHeading';
 import { Vector3 } from 'three';
@@ -15,40 +15,54 @@ function SourceEditor() {
     isValid: boolean;
     atoms?: Atom[];
   };
-  
+
   const parseXYZ = (text: string): ParsedXYZResult => {
-    const FIRST_LINES = 2;
+    const FIRST_TWO_LINES = 2;
     const lines = text.split('\n');
     const atomCount = parseInt(lines[0].trim(), 10);
     const newAtoms: Atom[] = [];
-  
-    if (isNaN(atomCount) || lines.length < atomCount + FIRST_LINES) {
-      return { isValid: false };
+
+    if (isNaN(atomCount) || lines.length < atomCount + FIRST_TWO_LINES) {
+      return { isValid: false }; //Must be the exact amount of lines 
     }
-  
-    for (let i = FIRST_LINES; i < atomCount + FIRST_LINES; i++) {
+
+    for (let i = FIRST_TWO_LINES; i < atomCount + FIRST_TWO_LINES; i++) {
       const line = lines[i].split(/\s+/);
-      if (line.length < 4) {
+      if (line.length !== 4) { // "element, x, y, z" -- exactly 4 objects
         return { isValid: false };
       }
       const element = line[0]; //TODO: Add validation against elements enum
+
+      for (let j = 1; j < line.length; j++) {
+        const elementValue = line[j];
+        if (!/^-?\d*\.?\d+$/.test(elementValue)) {
+          return { isValid: false };
+        }
+      }
+
       const x = parseFloat(line[1]);
       const y = parseFloat(line[2]);
       const z = parseFloat(line[3]);
       if (isNaN(x) || isNaN(y) || isNaN(z)) {
         return { isValid: false };
       }
-  
+
       newAtoms.push({
-        id: i - FIRST_LINES,
+        id: i - FIRST_TWO_LINES,
         element: element,
         position: new Vector3(x, y, z),
       });
     }
-  
+
     return { isValid: true, atoms: newAtoms };
   };
-  
+
+  useEffect(() => {
+    const parsedResult = parseXYZ(source);
+    setIsValidXYZFormat(parsedResult.isValid);
+  }
+  );
+
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newValue = event.target.value;
     setSource(newValue);
@@ -64,31 +78,31 @@ function SourceEditor() {
   };
 
   const handleSave = () => {
-      //TODO: save to local storage 
+    //TODO: save to local storage 
   }
 
   return (
     <Card className={styles.SourceEditor}>
       <VStack>
         <ViewHeading>
-        <h4>Source editor</h4>
+          <h4>Source editor</h4>
         </ViewHeading>
 
         <ControlGroup>
-          <InputGroup readOnly={true} value={sourceName} fill={true} rightElement={<Button text='Save' onClick={handleSave}/>}/>
+          <InputGroup readOnly={true} value={sourceName} fill={true} rightElement={<Button text='Save' onClick={handleSave} />} />
           <Tag
             style={{ width: '30%' }}
             intent={isValidXYZFormat ? 'success' : 'warning'}
-            >
+          >
             {isValidXYZFormat ? 'Correct XYZ pattern' : 'Wrong XYZ pattern'}
           </Tag>
         </ControlGroup>
-          <TextArea
-            id='text-editor'
-            className={styles.TextEditor}
-            value={source}
-            onChange={handleChange}
-          />
+        <TextArea
+          id='text-editor'
+          className={styles.TextEditor}
+          value={source}
+          onChange={handleChange}
+        />
       </VStack>
     </Card>
   )
