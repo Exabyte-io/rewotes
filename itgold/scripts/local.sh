9b65b37960522d8386b402291d090cbb3f07d790
Serge Kurenkov <serge@filefinder.io>
2023-04-15 02:35:19 -0700

    ansible hosts initialization from terraform

--- itgold/scripts/local.sh
+++ itgold/scripts/local.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 echo 'Checkout projects: local'
-ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/terraform-apply.yml $1 $2 $3 $4
-ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/provision.yml $1 $2 $3 $4
-ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/quantum_espresso.yml $1 $2 $3 $4
-#ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/exabyte.yml $1 $2 $3 $4
-#ansible-playbook -i ../ansible/inventory/dev.yml ../ansible/playbooks/terraform-destroy.yml $1 $2 $3 $4
+#ansible-playbook ../ansible/playbooks/terraform-apply.yml $1 $2 $3 $4
+ansible-playbook ../ansible/playbooks/terraform-collect.yml $1 $2 $3 $4
+#ansible-playbook ../ansible/playbooks/provision.yml $1 $2 $3 $4
+#ansible-playbook ../ansible/playbooks/quantum_espresso.yml $1 $2 $3 $4
+#ansible-playbook ../ansible/playbooks/exabyte.yml $1 $2 $3 $4
+#ansible-playbook ../ansible/playbooks/terraform-destroy.yml $1 $2 $3 $4
