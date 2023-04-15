deab1f72dbca0f0440e9d79819d1cc5ff2e16a5d
Serge Kurenkov <serge@filefinder.io>
2023-04-15 02:38:47 -0700

    terraform initialization

--- itgold/scripts/local.sh
+++ itgold/scripts/local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'Checkout projects: local'
-#ansible-playbook ../ansible/playbooks/terraform-apply.yml $1 $2 $3 $4
+ansible-playbook ../ansible/playbooks/terraform-apply.yml $1 $2 $3 $4
 ansible-playbook ../ansible/playbooks/terraform-collect.yml $1 $2 $3 $4
 #ansible-playbook ../ansible/playbooks/provision.yml $1 $2 $3 $4
 #ansible-playbook ../ansible/playbooks/quantum_espresso.yml $1 $2 $3 $4
--- itgold/workspace/README.md
+++
@@ -1,2 +0,0 @@
-# Overview
-This is the working folder for projects that is going to be checked out by the pipeline
