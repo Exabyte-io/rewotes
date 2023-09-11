38fade1f14b6bf5f9b3a3ca5c7a9e495e4edb580
rymo1354 <rymo1354@colorado.edu>
2023-09-11 09:23:27 -0600

    Improved modularity; added constructors.py

--- rymo1354/run.py
+++ rymo1354/run.py
@@ -1,6 +1,6 @@
 from ase.calculators.vasp import Vasp 
 from ase.io import read 
-from convergence.tracker import ConvergenceHandler
+from convergence.convergence import KptsConvergenceTracker 
 import argparse
 import os
 
@@ -10,6 +10,10 @@ def argument_parser():
         '-d', '--directory', help='Run directory with VASP POSCAR, KPOINTS, INCAR and POTCAR files', required=True)
     parser.add_argument(
         '-t', '--threshold', help='Threshold to meet for KPOINTS convergence; default is VASP total energy', type=float)
+    parser.add_argument(
+        '-c', '--calculation_type', help='Type of periodic structure calculation, i.e., VASP, Espresso, etc.', required=True)
+    parser.add_argument(
+        '-p', '--convergence_property', help='Property to converge', required=True)
     args = parser.parse_args()
     return args
 
@@ -21,17 +25,12 @@ if __name__ == '__main__':
     partition = 'debug'
     nodes = 1
     account = 'custws'
-    run_command = 'srun --time %s --partition %s --nodes %s --account %s %s' % (time, partition, str(nodes), account, executable_path)
-
+    
+    vasp_run_command = 'srun --time %s --partition %s --nodes %s --account %s %s' % (time, partition, str(nodes), account, executable_path)
+    commands_dct = {'VASP': vasp_run_command}
+    
     args = argument_parser()
-    ase_atoms = read(os.path.join(args.directory, 'POSCAR'))
-    calc = Vasp(command=run_command, directory=args.directory)
-    calc.read_incar(os.path.join(args.directory, 'INCAR'))
-    calc.read_kpoints(os.path.join(args.directory, 'KPOINTS'))
-    calc.read_potcar(os.path.join(args.directory, 'POTCAR'))
-    starting_kpoints = [1, 1, 1]
-
-    ch = ConvergenceHandler(ase_atoms, calc, starting_kpoints, args.threshold)
-    converged_kpoints, convergence_table = ch.converge_kpoints()
+    kpts_convergence = KptsConvergenceTracker(args.convergence_property, args.threshold)
+    converged_kpoints, convergence_table = kpts_convergence.run(args.calculation_type, args.directory, run_command=commands_dct[args.calculation_type])
     print(convergence_table)
     print('Converged with %s at property difference threshold = %s' % (str(converged_kpoints), str(args.threshold)))
