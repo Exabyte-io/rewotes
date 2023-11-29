86d4b3a7cb0ee98e74f7b0c1c6a1c431a1c27a49
JiQi535 <j1qi@eng.ucsd.edu>
2023-11-29 01:22:11 -0500

    update default featurizer to StructureFeaturizer

--- JiQi535/rewotesMLpredictor/predictor.py
+++ JiQi535/rewotesMLpredictor/predictor.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.pipeline import Pipeline
 from typing import Union
 from .featurizer import CompositionFeaturizer, StructureFeaturizer
@@ -11,13 +11,13 @@ class Predictor(Pipeline):
 
     def __init__(
             self,
-            featurizer: Union[CompositionFeaturizer, StructureFeaturizer] = CompositionFeaturizer(),
-            model=LinearRegression(),
+            featurizer: Union[CompositionFeaturizer, StructureFeaturizer] = StructureFeaturizer(),
+            model=DecisionTreeRegressor(),
     ):
         """
         Initialize the ML property predictors.
-        :param featurizer: CompositionFeaturizer or StructureFeaturizer. Default is CompositionFeaturizer.
-        :param model: The regression model in sklearn to use. Default is LinearRegression model.
+        :param featurizer: CompositionFeaturizer or StructureFeaturizer. Default is StructureFeaturizer.
+        :param model: The regression model in sklearn to use. Default is DecisionTreeRegressor.
         """
         self.featurizer = featurizer
         self.model = model
