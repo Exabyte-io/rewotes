02f0ba05b900db48eb9482b251778268a159a72e
John Gedeon <john@gedeons.com>
2024-02-10 00:37:25 -0800

    correct configuration to avoid e2e tests in unit tests

--- gidjin/vite.config.ts
+++ gidjin/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    testMatch: ['./src/**/*.test.tsx?'],
+    include: ['src/**/*.test.{js,ts,jsx,tsx}'],
     setupFiles: './tests.setup.ts'
   }
 })
