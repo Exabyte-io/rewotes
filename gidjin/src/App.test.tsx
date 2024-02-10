89eef581d0d384b50c3f021b995757e8d378e2fb
John Gedeon <john@gedeons.com>
2024-02-09 18:30:50 -0800

    setup simple todo list with add & delete

--- gidjin/src/App.test.tsx
+++ gidjin/src/App.test.tsx
@@ -1,13 +1,46 @@
 import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from "./App";
 
-
 describe('App', () => {
-  test('renders headline', () => {
-    render(<App />);
-    const headline = screen.getByText(/Vite \+ React/i);
-    expect(headline).toBeInTheDocument();
+  describe('adding a todo', () => {
+    test('renders form', () => {
+      render(<App />);
+      expect(screen.getByTestId("add-input")).toBeVisible();
+      expect(screen.getByTestId("add-button")).toBeVisible();
+    });
+
+    test('adds a todo', async () => {
+      render(<App />);
+      const input = screen.getByTestId("add-input");
+      const button = screen.getByTestId("add-button");
+      fireEvent.change(input, { target: { value: "test" } });
+      await button.click();
+      expect(screen.getByTestId("item-0")).toBeVisible();
+      expect(screen.getByTestId("item-0").textContent).toEqual("0. testDelete");
+    });
+  });
+
+  describe('deleting a todo', () => {
+    test('renders delete button', async () => {
+      render(<App />);
+      const input = screen.getByTestId("add-input");
+      const button = screen.getByTestId("add-button");
+      fireEvent.change(input, { target: { value: "test" } });
+      await button.click();
+      expect(screen.getByTestId("delete-button-0")).toBeVisible();
+    });
+
+    test('removes a todo', async () => {
+      render(<App />);
+      const input = screen.getByTestId("add-input");
+      const button = screen.getByTestId("add-button");
+      fireEvent.change(input, { target: { value: "test" } });
+      await button.click();
+      expect(screen.getByTestId("delete-button-0")).toBeVisible();
+      await screen.getByTestId("delete-button-0").click();
+      expect(screen.queryByTestId("item-0")).toBeNull();
+    });
   });
 });
 
--- gidjin/src/App.tsx
+++ gidjin/src/App.tsx
@@ -1,37 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([] as string[])
+  const [input, setInput] = useState("")
+
+  const addTodo = () => {
+    setTodo([...todo, input])
+    setInput("")
+  }
+
+  const onChange = (e: {target: {value: string}}) => {
+    setInput(e.target.value)
+  }
+
+  const onDelete = (idxToRemove: number) => {
+    setTodo(todo.filter((_item, idx) => idx !== idxToRemove))
+  }
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        H1 that uses tailwindcss
+        Simple TODO app for demonstration purposes
       </h1>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <ul className="pb-5">
+        {
+          todo.map((item, idx) => {
+            return (
+              <li data-testid={`item-${idx}`} key={idx}>
+                {idx}. {item}
+                <button data-testid={`delete-button-${idx}`} onClick={() => onDelete(idx)}>Delete</button>
+              </li>)
+          })
+        }
+      </ul>
 
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input data-testid="add-input" type="text" onChange={onChange}value={input}/>
+      <button data-testid="add-button" onClick={addTodo}>Add</button>
+
     </>
   )
 }
