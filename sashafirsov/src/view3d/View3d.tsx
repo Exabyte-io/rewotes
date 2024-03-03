bec778c1086fbad38457260c910d343802f558ac
Sasha Firsov <suns@firsov.net>
2024-03-02 22:14:30 -0800

    View3d spheres render, light adjusted

--- sashafirsov/src/view3d/View3d.tsx
+++ sashafirsov/src/view3d/View3d.tsx
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
 
+import styles from './View3d.module.css';
+
 function Box(props: ThreeElements['mesh']) {
     const ref = useRef<THREE.Mesh>(null!);
     const [hovered, hover] = useState(false);
@@ -21,14 +23,35 @@ function Box(props: ThreeElements['mesh']) {
     );
 }
 
+
+function Sphere(props: ThreeElements['mesh']) {
+    const ref = useRef<THREE.Mesh>(null!);
+    const [hovered, hover] = useState(false);
+    const [clicked, click] = useState(false);
+    useFrame((state, delta) => (ref.current.rotation.x += delta));
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}>
+            <sphereGeometry args={[1, 32, 32]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    );
+}
+
 export default function View3d() {
     return (
-        <div>
+        <div className={styles.content}>
             <Canvas>
-                <ambientLight />
+                <hemisphereLight position={[0, 1, 0 ]} args={[0xffffff, 0x888888, 3 ]}/>
                 <pointLight position={[10, 10, 10]} />
                 <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
+                <Sphere position={[1.2, 0, 0]} />
+
             </Canvas>
         </div>
     );
