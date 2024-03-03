b03c992a833e08a138159372f0f69a5ba936405d
Sasha Firsov <suns@firsov.net>
2024-03-02 20:38:55 -0800

    Initial View3d and Editor paint

---
+++ sashafirsov/src/view3d/View3d.tsx
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+
+function Box(props: ThreeElements['mesh']) {
+    const ref = useRef<THREE.Mesh>(null!);
+    const [hovered, hover] = useState(false);
+    const [clicked, click] = useState(false);
+    useFrame((state, delta) => (ref.current.rotation.x += delta));
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    );
+}
+
+export default function View3d() {
+    return (
+        <div>
+            <Canvas>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+            </Canvas>
+        </div>
+    );
+}
